56f290ca4c1abbddd60a0bf304a2cb7f2398f8a3
jeevaanandh <anandhjeeva215@gmail.com>
2026-03-12 02:14:19 +0530

    The ESP sends data correctly to the python server. The python server correctly uses the Isolation Forest model to get the predictions

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from FOGInference import process_new_reading
+
 app = FastAPI()
+
+window_readings=[]
 
 class Item(BaseModel):
     temperature: float
@@ -12,5 +16,14 @@ class Item(BaseModel):
 
 @app.post("/items")
 async def create_item(item: Item):
+    global window_readings
     print(item)
+
+    window_readings.append(item)
+    if(len(window_readings)==5):
+        res=process_new_reading(window_readings)
+        print(res)
+        window_readings=[]
+
+    
     return {"message": "Item received", "item": item}
